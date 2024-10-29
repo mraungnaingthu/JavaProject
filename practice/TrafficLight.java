2f5a22fad835471ea53eaae0b0758d92065947ba
aungnaingthu <aungnaingthu176440@gmail.com>
2024-10-29 14:38:44 +0630

    Add practive project under com.mm.aungnaingthu

---
+++ practice/TrafficLight.java
@@ -0,0 +1,30 @@
+package com.mm.aungnaingthu.practice;
+
+public enum TrafficLight {
+    RED(30), YELLOW(5), GREEN(25);
+
+    private final int duration;
+
+    TrafficLight(int duration){
+        this.duration = duration;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public TrafficLight next() throws IllegalAccessException {
+        switch (this){
+            case RED -> {
+                return GREEN;
+            }
+            case YELLOW -> {
+                return RED;
+            }
+            case GREEN -> {
+                return YELLOW;
+            }
+            default -> throw new IllegalAccessException("Unexpected value: " +this);
+        }
+    }
+}
